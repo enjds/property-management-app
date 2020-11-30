27c8c081b562546179dec94a32f24c81d4f622ef
Benjo Delos Santos <delossantosbenjo@gmail.com>
2020-11-30 23:59:08 +0800

    [CHORE]: Add project folder structure

---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+// to simplify imports for screen
+// this will be the import for routes
+export * from './Client';
+export * from './Expenses';
+export * from './Payments';
+export * from './PropertyList';
