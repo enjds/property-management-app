f0ec21d3f165b9629bcc0ecbef881cd5a912ea89
Benjo Delos Santos <delossantosbenjo@gmail.com>
2020-12-02 23:59:51 +0800

    [CHORE]: Add assets constant values

---
+++ src/constants/assets.js
@@ -0,0 +1,7 @@
+export const icons = {
+    properties: require('../assets/img/interface/search.png'),
+    plus: require('../assets/img/interface/plus.png'),
+    clients: require('../assets/img/interface/layout.png'),
+    expenses: require('../assets/img/general/chart-pie.png'),
+    payments: require('../assets/img/files/invoice.png')
+};
