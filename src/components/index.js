02eb4da8974e9d7da2a1487a51f35a7c8eb99efb
Benjo Delos Santos <delossantosbenjo@gmail.com>
2020-12-03 00:01:24 +0800

    [CHORE]: Add barrel file for components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+/**
+ * Components Barrel is a collection of all components
+ * used by the app. This simplifies the import path
+ */
+import BottomTabNavigator from './BottomTabNavigator';
+
+export {
+    BottomTabNavigator
+};
