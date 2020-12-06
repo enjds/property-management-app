ba205d0b8657509ea08b454cdd110f8958fa3642
Benjo Delos Santos <delossantosbenjo@gmail.com>
2020-12-07 00:32:53 +0800

    [REFACTOR]: Add mock data

---
+++ src/constants/mocks.js
@@ -0,0 +1,42 @@
+export const MOCK_DATA = [
+    {
+      id: '1',
+      price: '$2,200',
+      imageUrl: 0,
+      address: '635 Jacobs Stream',
+      bed: 1,
+      toilet: 1,
+      isDue: false,
+      floorArea: '24 ft2'
+    },
+    {
+      id: '2',
+      price: '$12,200',
+      imageUrl: 1,
+      address: '2805 Doyle Grove',
+      bed: 3,
+      toilet: 3,
+      isDue: true,
+      floorArea: '48 ft2'
+    },
+    {
+      id: '3',
+      price: '$5,400',
+      imageUrl: 2,
+      address: '101 Main Street',
+      bed: 3,
+      toilet: 3,
+      isDue: false,
+      floorArea: '48 ft2'
+    },
+    {
+      id: '4',
+      price: '$2,700',
+      imageUrl: 3,
+      address: '522 Joane Park',
+      bed: 2,
+      toilet: 5,
+      isDue: true,
+      floorArea: '2,401 ft2'
+    }
+  ];
